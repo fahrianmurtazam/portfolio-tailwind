f729fb39cc5e2a58dedcb1f93433c86c46c88a34
fahrianmurtazam <fahrian22@mhs.usk.ac.id>
2023-08-11 18:40:02 +0700

    portfolio tailwind css

---
+++ dist/js/script.js
@@ -0,0 +1,50 @@
+// navbar fix
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const navfix = header.offsetTop;
+
+  if (window.pageYOffset > navfix) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+// hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// klik diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindah posisi toggle sesuai mode
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
